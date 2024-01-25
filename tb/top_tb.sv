2cb922eda4fa46fee152aaf21b6455cca87ec7f2
Gorruy <vladimirmimikin@gmail.com>
2024-01-25 15:22:11 +0300

    + settle task

--- tb/top_tb.sv
+++ tb/top_tb.sv
@@ -2,12 +2,12 @@ module top_tb;
 
   parameter NUMBER_OF_TEST_RUNS            = 100;
 
-  parameter BLINK_HALF_PREIOD_MS           = 10;
+  parameter BLINK_HALF_PERIOD_MS           = 10;
   parameter BLINK_GREEN_TIME_TICK          = 2;
   parameter RED_YELLOW_MS                  = 5;
 
   localparam G_Y_TOGGLE_HPERIOD_CLK_CYCLES = BLINK_HALF_PERIOD_MS * 2; 
-  localparam G_BLINK_CLK_CYCLES            = BLINK_GREEN_TIME_TICK * G_Y_TOGGLE_CLK_CYCLES * 2;
+  localparam G_BLINK_CLK_CYCLES            = BLINK_GREEN_TIME_TICK * G_Y_TOGGLE_HPERIOD_CLK_CYCLES * 2;
   localparam RED_YELLOW_CLK_CYCLES         = RED_YELLOW_MS * 2;
 
   localparam CMD_SIZE                      = 3;
@@ -48,33 +48,35 @@ module top_tb;
     end
 
   traffic_lights #(
-    .BLINK_HALF_PREIOD_MS  ( 10         ),
-    .BLINK_GREEN_TIME_TICK ( 2          ),
-    .RED_YELLOW_MS         ( 5          )
+    .BLINK_HALF_PERIOD_MS  ( 10          ),
+    .BLINK_GREEN_TIME_TICK ( 2           ),
+    .RED_YELLOW_MS         ( 5           )
   ) traffic_lights (
-    .clk_i                 ( clk_i       ),
-    .srst_i                ( srst_i      ),
+    .clk_i                 ( clk         ),
+    .srst_i                ( srst        ),
     .cmd_type_i            ( cmd_type_i  ),
     .cmd_valid_i           ( cmd_valid_i ),
     .cmd_data_i            ( cmd_data_i  ),
     .red_o                 ( red_o       ),
     .yellow_o              ( yellow_o    ),
     .green_o               ( green_o     )
-  )
+  );
 
   typedef struct { logic [PERIOD_SIZE - 1:0] yellow_period;
                    logic [PERIOD_SIZE - 1:0] red_period;
                    logic [PERIOD_SIZE - 1:0] green_period;
                    int                       off_time;
                    int                       notransition_time;
-                   int                       normal_mode_time; } session_t;
+                   int                       normal_time_after_to;
+                   int                       normal_time_after_set; } session_t;
 
   mailbox #( session_t ) generated_sessions = new();
 
-  function void put_settings ( input logic [PERIOD_SIZE - 1:0] yellow_period,
-                                     logic [PERIOD_SIZE - 1:0] red_period, 
-                                     logic [PERIOD_SIZE - 1:0] green_period 
-                                     logic [CMD_SIZE - 1:0]    cmd_type );
+  task put_settings ( input logic [PERIOD_SIZE - 1:0] green_period,
+                            logic [PERIOD_SIZE - 1:0] red_period, 
+                            logic [PERIOD_SIZE - 1:0] yellow_period, 
+                            logic [CMD_SIZE - 1:0]    cmd_type 
+                    );
     cmd_type_i  = cmd_type;
     cmd_valid_i = 1'b1;
 
@@ -98,53 +100,71 @@ module top_tb;
     cmd_valid_i = 1'b0;
     cmd_data_i  = '0;
 
-  endfunction
+  endtask
 
   function void display_error ( );
 
   endfunction
 
-  task settle_sessions ( mailbox #( data_t ) generated_sessions );
+  task settle_sessions ( mailbox #( session_t ) generated_sessions );
+    session_t session_to_settle;
 
+    while ( generated_sessions.num() )
+      begin
+        generated_sessions.get( session_to_settle );
+
+        put_settings( '0, '0, '0, (CMD_SIZE)'(1) );
+        ##(session_to_settle.off_time);
+
+        put_settings( '0, '0, '0, (CMD_SIZE)'(0) );
+        ##(session_to_settle.normal_time_after_to);
+
+        put_settings( '0, '0, '0, (CMD_SIZE)'(2) );
+        put_settings( session_to_settle.green_period, '0, '0, (CMD_SIZE)'(3) );
+        put_settings( '0, session_to_settle.red_period, '0, (CMD_SIZE)'(4) );
+        put_settings( '0, '0, session_to_settle.yellow_period, (CMD_SIZE)'(5) );
+        ##(session_to_settle.notransition_time);
+
+        put_settings( '0, '0, '0, (CMD_SIZE)'(1) );
+        ##(session_to_settle.normal_time_after_set);
+        
+      end
   endtask
 
-  task generate_sessions ( mailbox #( data_t ) generated_sessions );
+  task generate_sessions ( mailbox #( session_t ) generated_sessions );
 
     session_t generated_session;
 
     repeat ( NUMBER_OF_TEST_RUNS )
       begin
-        generated_session.yellow_period     = $urandom_range( 100, 0 );
-        generated_session.red_period        = $urandom_range( 100, 0 );
-        generated_session.green_period      = $urandom_range( 100, 0 );
-        generated_session.off_time          = $urandom_range( 100, 0 );
-        generated_session.notransition_time = $urandom_range( 100, 0 );
-        generated_session.normal_mode_time  = $urandom_range( 10000, 0 );
+        generated_session.yellow_period         = $urandom_range( 100, 0 );
+        generated_session.red_period            = $urandom_range( 100, 0 );
+        generated_session.green_period          = $urandom_range( 100, 0 );
+        generated_session.off_time              = $urandom_range( 100, 0 );
+        generated_session.notransition_time     = $urandom_range( 100, 0 );
+        generated_session.normal_time_after_set = $urandom_range( 100, 0 );
+        generated_session.normal_time_after_to  = $urandom_range( 100, 0 );
 
-        generate_sessions.put( generated_session );
+        generated_sessions.put( generated_session );
       end
 
   endtask
 
-  task read_data ( mailbox #( data_t ) output_data );
+  task read_data ( mailbox #( session_t ) output_data );
 
   endtask
 
   initial begin
-    data           <= '0;
-    data_val_i     <= 1'b0;
     test_succeed   <= 1'b1;
 
     $display("Simulation started!");
-    generate_transactions( generated_data );
+    generate_sessions( generated_sessions );
     wait( srst_done === 1'b1 );
 
     fork
-      send_data( input_data, generated_data );
-      read_data( output_data );
+      settle_sessions( generated_sessions );
     join
 
-    compare_data( input_data, output_data );
     $display("Simulation is over!");
     if ( test_succeed )
       $display("All tests passed!");
